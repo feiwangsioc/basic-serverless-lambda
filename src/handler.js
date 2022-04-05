c51be10b08f6836b35c94315e4189c288f22ce50
Fei Wang <fei.wang@spatialfront.com>
2022-04-05 10:26:59 -0400

    update 1

--- src/handler.js
+++ src/handler.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports.generateRandomNumber = event => {
+module.exports.generateRandomNumber = async event => {
   const randomNumber = parseInt(Math.random() * 100);
   console.log("The random generated integer is: ", randomNumber);
   return randomNumber;
